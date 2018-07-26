7d0ff339d8f75c069f055e1491a04ccdc62aad98
Ivan Vodzich <ivan_vodzich@epam.com>
2018-07-26 21:03:45 +0300

    #version9

---
+++ examples/version9/app.js
@@ -0,0 +1,35 @@
+let canvas = new fabric.Canvas('canvas')
+
+var refresh = function () {
+  var data = JSON.stringify(canvas)
+  canvas.loadFromJSON(data, function () {
+    canvas.renderAll()
+  })
+}
+const refreshButton = document.querySelector('#refreshButton')
+refreshButton.addEventListener('click', refresh)
+
+let bubbleOptions = {
+  x: 200,
+  y: 200,
+  w: 200,
+  h: 80,
+  lineWidth: 2,
+  lineColor: 'green',
+  backgroundColor: 'red'
+}
+
+let pointerOptions = {
+  x: 150,
+  y: 150,
+  radius: 8,
+  color: 'blue'
+}
+
+extendFabricPathBubble(fabric, canvas)
+let i = 3;
+while (i--) {
+  Bubble.create(fabric, canvas, bubbleOptions, pointerOptions)
+  bubbleOptions.x += 250;
+  pointerOptions.x += 250;
+}
